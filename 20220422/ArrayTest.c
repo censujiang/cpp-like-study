d07cc6cbe12f6d5800287f042d20a53a74318385
江程训 <dfjsiou@qq.com>
2022-04-22 14:40:26 +0800

    数组

---
+++ 20220422/ArrayTest.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int a[6] = {1,2,3,4,5,6};
+    int i=0;
+    for(i=0;i<6;i++){
+        printf("%d\n",a[i]);
+    }
+    return 0;
+}
--- Training/student.c
+++ Training/student.c
@@ -14,7 +14,20 @@ int main(){
         printf("*****************************************\n");
         printf("请选择1~7：");
         scanf("%d",&choice);
-        if (choice == 7){
+        if (choice == 1){
+            printf("您选择的是:1.输入学生成绩\n");
+        }else if(choice == 2){
+            printf("您选择的是:2.排序学生成绩\n");
+        }else if(choice == 3){
+            printf("您选择的是:3.显示学生成绩\n");
+        }else if(choice == 4){
+            printf("您选择的是:4.修改学生成绩\n");
+        }else if(choice == 5){
+            printf("您选择的是:5.查询学生成绩\n");
+        }else if(choice == 6){
+            printf("您选择的是:6.统计学生成绩\n");
+        }else if(choice == 7){
+            printf("您选择的是:7.退出系统\n");
             break;
         }
     }
