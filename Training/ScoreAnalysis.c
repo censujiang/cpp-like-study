b2f54ada79c7b21b6b829306c3fa8e04ffd1b267
江程训 <dfjsiou@qq.com>
2022-05-09 21:12:51 +0800

    refresh

--- Training/ScoreAnalysis.c
+++ Training/ScoreAnalysis.c
@@ -2,7 +2,7 @@
 int main()
 {
     int scores[3][10] = {0};
-    printf("请输入3个班级10个学生的成绩；\n");
+    printf("请输入3个班级10个学生的成绩：\n");
     int i, j;
     for (i = 0; i < 3; i++)
     {
@@ -12,12 +12,16 @@ int main()
         }
     }
     printf("以下是分析结果；\n");
-    printf("%10s\t""班级的班号");
-    printf("%10s\t""平均成绩");
-    printf("%10s\t""最高分");
-    printf("%10s\t""最高分序号");
-    printf("========================================================");
-    int avg, max, maxIndex, sum;
+    printf("%10s\t", "班级的班号");
+    printf("%10s\t", "平均成绩");
+    printf("%10s\t", "最高分");
+    printf("%10s\n", "最高分序号");
+    printf("==========================================================\n");
+    int max, sum, maxIndex;
+    float avg;
+    int allMax = 0, allMaxIndex = 0, classNum = 0;
+    int classNum2 = 0;
+    float avgMax = 0;
     for (i = 0; i < 3; i++)
     {
         sum = 0;
@@ -32,11 +36,26 @@ int main()
             }
             sum += scores[i][j];
         }
+        avg = (float)sum / 10;
         printf("%10d\t", i + 1);
-        printf("%10d.2f\t", ((float)sum / 10));
+        printf("%10.2f\t", ((float)sum / 10));
         printf("%10d\t", max);
-        printf("%10d\t", maxIndex + 1);
+        printf("%10d\n", maxIndex + 1);
+        if (max > allMax)
+        {
+            allMax = max;
+            allMaxIndex = maxIndex;
+            classNum = i;
+        }
+        if (avg > avgMax)
+        {
+            avgMax = avg;
+            classNum2 = i;
+        }
     }
-    system("pause");
+    printf("最高分为%d班，序号是%d,分数是%d\n",
+           classNum + 1, allMaxIndex + 1, allMax);
+    printf("平均成绩最高的是%d班，最高平均成绩是%.2f\n",
+           classNum2 + 1, avgMax);
     return 0;
-} 
+}
