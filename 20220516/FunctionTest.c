41365d6bc9627496e0fd2e72b91299d9d7f49ec5
江程训 <dfjsiou@qq.com>
2022-05-16 09:19:08 +0800

    acg & max

--- 20220516/FunctionTest.c
+++ 20220516/FunctionTest.c
@@ -1,14 +1,25 @@
-#include <stdio>
+#include <stdio.h>
 int min(int x, int y){
     if(x<y)
         return x;
     else
         return y;
 }
+int max(int x, int y){
+    if(x>y)
+        return x;
+    else
+        return y;
+}
+float avg(int x, int y){
+    return (x+y)/2.0;
+}
 int main(){
-    int a,b,c;
+    int a,b,c,d,e;
     scanf("%d %d",&a,&b);
     c = min(a,b);
-    printf("min is %d",c);
+    d = max(a,b);
+    e = avg(a,b);
+    printf("min is %d,max is %d,avg is %d",c ,d ,e);
     return 0;
 }
